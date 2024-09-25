4a89192da3f24232769d96da469223023d11425a
Jonas <tuyendev@gmail.com>
2024-09-25 16:01:02 +0700

    update script to ensure process wait util connect to resources

--- pgsync/entrypoint.sh
+++ pgsync/entrypoint.sh
@@ -1,9 +1,14 @@
 #! /bin/bash
-echo -e "\nSleep 10 seconds before [Re]Bootstrap . . .\n"
-sleep 10
+echo -e "\nProcess is pending util database, redis, opensearch is up."
+wait-for-it $PG_HOST:$PG_PORT -t 60
+wait-for-it $REDIS_HOST:$REDIS_PORT -t 60
+wait-for-it $ELASTICSEARCH_HOST:$ELASTICSEARCH_PORT -t 60
+
+echo -e "\n [Re]Bootstrap . . .\n"
 bootstrap --config /etc/schema.json
 echo -e "\n[Re]Bootstrap is done\n"
 
 echo -e "\nSynchonize will be start after 5 seconds . . .\n"
 sleep 5
+
 pgsync --config /etc/schema.json --daemon
